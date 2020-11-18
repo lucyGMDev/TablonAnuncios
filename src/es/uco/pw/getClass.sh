a3a4d4d023fd63c4c2373288285e8e0eb9343f3e
Lucia <lucygm.dev@gmail.com>
2020-11-18 11:17:19 +0100

    Añadiendo .class

---
+++ src/es/uco/pw/getClass.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+javac -d ../../../../class/ data/dao/Anuncios/AnuncioDAO.java business/Anuncios/*.java business/DTO/DTOAnuncio/*.java business/Usuario/Contacto.java business/Usuario/GestorContactos.java data/dao/common/DAO.java data/dao/Usuario/ContactoDAO.java data/dao/Intereses/InteresesDAO.java business/DTO/DTOUsuario/ContactoDTO.java display/TablonAnuncios.java
+
